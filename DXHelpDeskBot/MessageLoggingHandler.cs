6e10fedb966fdd9112891006d63b5afbee5aef38
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-15 18:09:53 -0700

    message Logging handler

---
+++ DXHelpDeskBot/MessageLoggingHandler.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXHelpDeskBot
+{
+    public class MessageLoggingHandler : MessageHandler
+	{
+		protected override async Task IncommingMessageAsync(string correlationId, string requestInfo, byte[] message)
+		{
+			await Task.Run(() =>
+				Console.WriteLine(string.Format("{0} - Request: {1}\r\n{2}", correlationId, requestInfo, Encoding.UTF8.GetString(message))));
+		}
+
+
+		protected override async Task OutgoingMessageAsync(string correlationId, string requestInfo, byte[] message)
+		{
+			await Task.Run(() =>
+				Console.WriteLine(string.Format("{0} - Response: {1}\r\n{2}", correlationId, requestInfo, Encoding.UTF8.GetString(message))));
+		}
+	}
+}
