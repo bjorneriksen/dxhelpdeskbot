ffc75b3a273731baca63605d260aa19ddb25eee3
Thiago Almeida <thalme@microsoft.com>
2017-05-15 23:16:03 -0700

    Adding crawler

---
+++ DXHelpDeskBot.Crawler/CrawlAzureDocs.cs
@@ -0,0 +1,127 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Abot;
+using Abot.Poco;
+using Abot.Crawler;
+using System.Net;
+using System.Linq;
+
+namespace DXHelpDeskBot.Crawler
+{
+    public static class CrawlAzureDocs
+    {
+        private static string crawlSiteBase = Environment.GetEnvironmentVariable("CrawlSiteBase");
+        private static TraceWriter _log;
+        private static IAsyncCollector<CrawlerEntry> _outDocument;
+
+        //Crawls every 6 hours
+        [FunctionName("DxHelpDeskBotCrawler")]
+        public static void Run([TimerTrigger("0 0 */6 * * *")]TimerInfo myTimer,
+                               [DocumentDB("pages", "crawledpages", Id = "id", PartitionKey = "Service", ConnectionStringSetting = "DxHelpDeskBotCrawlerData")] IAsyncCollector<CrawlerEntry> outDocument,
+                               TraceWriter log)
+        {
+            try
+            {
+                _log = log;
+                _outDocument = outDocument;
+                _log.Info($"DxHelpDeskBotCrawler Timer trigger function executed at: {DateTime.Now}");
+
+                CrawlConfiguration crawlConfig = new CrawlConfiguration();
+                crawlConfig.CrawlTimeoutSeconds = 100;
+                crawlConfig.MaxConcurrentThreads = 10;
+                crawlConfig.MaxPagesToCrawl = 20000;
+                crawlConfig.DownloadableContentTypes = "text/html, text/plain";
+                crawlConfig.IsExternalPageCrawlingEnabled = false;
+                crawlConfig.IsExternalPageLinksCrawlingEnabled = false;
+
+                PoliteWebCrawler crawler = new PoliteWebCrawler(crawlConfig);
+                crawler.PageCrawlStartingAsync += Crawler_ProcessPageCrawlStarting;
+                crawler.PageCrawlCompletedAsync += Crawler_ProcessPageCrawlCompleted;
+                crawler.PageCrawlDisallowedAsync += Crawler_PageCrawlDisallowed;
+                crawler.PageLinksCrawlDisallowedAsync += Crawler_PageLinksCrawlDisallowed;
+
+                _log.Info(crawlSiteBase + " crawler started...");
+                //This is synchronous, it will not go to the next line until the crawl has completed
+                CrawlResult result = crawler.Crawl(new Uri(crawlSiteBase));
+
+                if (result.ErrorOccurred)
+                    _log.Error($"Crawl of {result.RootUri.AbsoluteUri} completed with error: {result.ErrorException.Message}");
+                else
+                {
+                    _log.Info($"Crawl of {result.RootUri.AbsoluteUri} completed without error.");
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Failed crawl", ex, "DxHelpDeskBotCrawler");
+                throw;
+            }
+
+        }
+
+        static void Crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            _log.Info($"About to crawl link {pageToCrawl.Uri.AbsoluteUri} which was found on page {pageToCrawl.ParentUri.AbsoluteUri}");
+        }
+
+        static void Crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+                _log.Info($"Crawl of page failed {crawledPage.Uri.AbsoluteUri}");
+            else
+                _log.Info($"Crawl of page succeeded {crawledPage.Uri.AbsoluteUri}");
+
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                _log.Info($"Page had no content {crawledPage.Uri.AbsoluteUri}");
+
+            var htmlAgilityPackDocument = crawledPage.HtmlDocument; //Html Agility Pack parser
+            var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
+
+            var description = String.Empty;
+            var descriptionElement = angleSharpHtmlDocument.QuerySelectorAll("meta[name=\"description\"]")
+                        .FirstOrDefault();
+            if (descriptionElement != null)
+                description = descriptionElement.GetAttribute("content");
+
+            var service = String.Empty;
+            var serviceElement = angleSharpHtmlDocument.QuerySelectorAll("meta[name=\"ms.service\"]")
+                        .FirstOrDefault();
+            if (serviceElement != null)
+                service = serviceElement.GetAttribute("content");
+
+            var documentId = String.Empty;
+            var documentIdElement = angleSharpHtmlDocument.QuerySelectorAll("meta[name=\"document_id\"]")
+                        .FirstOrDefault();
+            if (documentIdElement != null)
+                documentId = documentIdElement.GetAttribute("content");
+
+            _outDocument.AddAsync(
+                new CrawlerEntry()
+                {
+                    id = documentId,
+                    URL = crawledPage.Uri.ToString(),
+                    Title = angleSharpHtmlDocument.Title,
+                    Description = description,
+                    Content = angleSharpHtmlDocument.Body.TextContent,
+                    Service = service,
+                    LastUpdated = DateTime.Now
+                });
+        }
+
+        static void Crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            _log.Info($"Did not crawl the links on page {crawledPage.Uri.AbsoluteUri} due to {e.DisallowedReason}");
+        }
+
+        static void Crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            _log.Info($"Did not crawl page {pageToCrawl.Uri.AbsoluteUri} due to {e.DisallowedReason}");
+        }
+    }
+}
---
+++ DXHelpDeskBot.Crawler/CrawlerEntry.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXHelpDeskBot.Crawler
+{
+    public class CrawlerEntry
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+        public string URL { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string Service { get; set; }
+        public DateTime LastUpdated  { get; set; }
+}
+}
