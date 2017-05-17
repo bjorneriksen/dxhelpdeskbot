df03b87eeb039ed78d4d4cd981354d450d2f1c1b
Ken Casada <kcasada@microsoft.com>
2017-05-17 11:45:35 -0700

    Azure Search Integration

---
+++ DXHelpDeskBot/Dialogs/DocumentationExplorerDialog.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using DXHelpDeskBot.Services;
+
+namespace DXHelpDeskBot.Dialogs
+{
+    public class DocumentationExplorerDialog: IDialog<object>
+    {
+        private readonly AzureSearchService searchService = new AzureSearchService();
+
+        public Task StartAsync(IDialogContext context)
+        {
+            return null;
+        }
+    }
+}
---
+++ DXHelpDeskBot/Models/SearchResult.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DXHelpDeskBot.Models
+{
+    [SerializePropertyNamesAsCamelCase]
+    public class SearchResult
+    {
+        public string id { get; set; }
+
+        [Key]
+        public string rid { get; set; }
+
+        public string URL { get; set; }
+
+        [IsSearchable, IsSortable]
+        public string Title { get; set; }
+
+        [IsSearchable]
+        public string Description { get; set; }
+
+        [IsSearchable]
+        public string Content { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable]
+        public string Service { get; set; }
+
+        [IsFilterable, IsSortable]
+        public DateTime? LastUpdated { get; set; }
+    }
+}
