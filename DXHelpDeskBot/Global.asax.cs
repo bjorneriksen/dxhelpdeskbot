f16346d2a956f9ab852a77d3b9cce96013adc448
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-19 05:17:30 -0700

    Adding AutoFac module for debugging activation requests

--- DXHelpDeskBot/Global.asax.cs
+++ DXHelpDeskBot/Global.asax.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Http;
+using Autofac;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace DXHelpDeskBot
 {
@@ -8,6 +10,10 @@ namespace DXHelpDeskBot
         {
 			GlobalConfiguration.Configuration.MessageHandlers.Add(new MessageLoggingHandler());
 			GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var cb = new ContainerBuilder();
+            cb.RegisterModule<LogRequestModule>();
+            cb.Update(Conversation.Container);
         }
     }
 }
--- DXHelpDeskBot/LogRequestModule.cs
+++ DXHelpDeskBot/LogRequestModule.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using Autofac;
+using Autofac.Core;
+
 namespace DXHelpDeskBot
 {
-    public class LogRequestModule
-    {
-        public LogRequestModule()
-        {
-        }
-    }
+	public class LogRequestModule : Module
+	{
+		public int depth = 0;
+
+		protected override void AttachToComponentRegistration(IComponentRegistry componentRegistry,
+															  IComponentRegistration registration)
+		{
+			registration.Preparing += RegistrationOnPreparing;
+			registration.Activating += RegistrationOnActivating;
+
+			base.AttachToComponentRegistration(componentRegistry, registration);
+		}
+
+		private string GetPrefix()
+		{
+			return new string('-', depth * 2);
+		}
+
+		private void RegistrationOnPreparing(object sender, PreparingEventArgs preparingEventArgs)
+		{
+			Console.WriteLine("{0}Resolving  {1}", GetPrefix(), preparingEventArgs.Component.Activator.LimitType);
+			depth++;
+		}
+
+		private void RegistrationOnActivating(object sender, ActivatingEventArgs<object> activatingEventArgs)
+		{
+			depth--;
+			Console.WriteLine("{0}Activating {1}", GetPrefix(), activatingEventArgs.Component.Activator.LimitType);
+		}
+	}
 }
